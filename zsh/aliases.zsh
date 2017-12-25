798fb267a3720bb83f33859f1689db7ccd8a2a7e
Urosh Trifunovic <nediarovic@gmail.com>
2017-12-25 23:46:21 +0100

    Adding aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,20 +1,24 @@
 # Filesystem
-alias ls='ls --color=auto'
-alias ll='ls -l'
-alias la='ls -la'
+alias ls="ls --color=auto"
+alias ll="ls -l"
+alias la="ls -la"
+alias e="exit"
 
 # Grep
-alias grep='grep --color=auto'
+alias grep="grep --color=auto"
 
 # Tmux
-alias ta='tmux new-session -A -s'
-alias t='tmux'
+alias ta="tmux new-session -A -s"
+alias t="tmux"
 
 # Docker
 alias d="docker"
 alias dc="docker-compose"
 alias dm="docker-machine"
 alias reboot_docker="dc stop && dc build --no-cache && dc up -d"
-alias df="docker logs -f"
 alias dt="docker logs -f --tail"
 
+# Git
+alias g="git"
+alias ga="git add"
+alias gc="git commit"
