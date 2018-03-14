4ba982c85b97c1e379967b480a81bbc01caacc06
Urosh Trifunovic <nediarovic@gmail.com>
2018-03-14 21:37:49 +0100

    Change double quotes to single quotes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,34 +1,36 @@
 # Filesystem
-alias ls="ls --color=auto"
-alias ll="ls -l"
-alias la="ls -la"
-alias e="exit"
+alias ls='ls --color=auto'
+alias ll='ls -l'
+alias la='ls -la'
+alias e='exit'
 alias mkcd='mkcd(){ mkdir -p "$1" && cd "$1" }'
 
+# Network
+myip='curl http://ipecho.net/plain; echo'
 # Grep
-alias grep="grep --color=auto"
+alias grep='grep --color=auto'
 
 # Tmux
-alias ta="tmux new-session -A -s"
-alias t="tmux"
+alias ta='tmux new-session -A -s'
+alias t='tmux'
 
 # Docker
-alias d="docker"
-alias dc="docker-compose"
-alias dm="docker-machine"
-alias reboot_docker="dc stop && dc build --no-cache && dc up -d"
-alias dt="docker logs -f --tail"
+alias d='docker'
+alias dc='docker-compose'
+alias dm='docker-machine'
+alias reboot_docker='dc stop && dc build --no-cache && dc up -d'
+alias dt='docker logs -f --tail'
 
 # Kubernetes
-alias k="kubectl"
-alias m="minikube"
-alias watch="watch " # So you can do use watch with alias k :)
+alias k='kubectl'
+alias m='minikube'
+alias watch='watch ' # So you can do use watch with alias k :)
 
 # Git
-alias g="git"
-alias ga="git add"
-alias gc="git commit"
+alias g='git'
+alias ga='git add'
+alias gc='git commit'
 
 # Maven
-alias mci="mvn clean install"
+alias mci='mvn clean install'
 
