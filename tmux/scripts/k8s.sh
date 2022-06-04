b1485097739e1cc3e283fbafba0f31564ec3ffbe
Urosh Trifunovic <nediarovic@gmail.com>
2022-06-04 20:55:13 +0200

    [tmux] Fix statusline adujstment

--- tmux/scripts/k8s.sh
+++ tmux/scripts/k8s.sh
@@ -3,5 +3,5 @@
 context="$(kubectl config current-context)"
 namespace="$(kubectl config view --minify --output 'jsonpath={..namespace}')"
 if [[ $context != "minikube" && $context != "" ]]; then
-    echo "k8s:$context [$namespace] "
+    echo "$context[$namespace] "
 fi
